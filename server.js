32d4d8e3778cc79b885b8c72f65dd43525c77d0c
Harsh <harshvardhansingh7525@gmail.com>
2021-05-22 17:04:11 +0530

    second cors

--- server.js
+++ server.js
@@ -1,47 +1,38 @@
-const express= require('express');
-
-
-const favicon= require('serve-favicon');
-const createError= require('http-errors');
-const path=require('path')
-const app=express();
-
-
-
-
+require('dotenv').config();
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
 
-app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static('public'));  //tell express to use static things like ,logo and css
-app.use(express.json());
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
+
+app.use(cors(corsOptions))
+app.use(express.static('public'));
+
 const connectDB = require('./config/db');
 connectDB();
 
-app.use(favicon(path.join(__dirname,'public', 'favicon.ico')));
-//template engine
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine','ejs');
-app.engine('html', require('ejs').renderFile);
+app.use(express.json());
 
-//routes
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+// Routes 
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
 
-//catch 404 and forward
 
-app.use((req,res, next)=>{
-    next(new createError.NotFound());
-});
-
-//error handler
-app.use((err,req,res, next)=>{
-    res.status(err.status || 500);
-    res.render('error.html',{err});
-});
-
-
-
-app.listen(PORT, ()=>{
-    console.log(`Listening on port ${PORT}`);
-})
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
